7064b25505a03f0b68a0041542a2de9d2821c594
Chaliana Rolon <chaliana@fb.com>
2018-06-26 18:53:35 -0700

    First commit

---
+++ SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  TipCalculator
+//
+//  Created by Chaliana Rolon on 6/26/18.
+//  Copyright © 2018 Chaliana Rolon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
